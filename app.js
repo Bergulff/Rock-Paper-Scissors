ede08e688a51ad08fa91ccf2b73536554f387cce
Bergulff <Bergulff@hotmail.com>
2022-04-08 20:22:07 +0200

    Added playerSelection

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Returning the computers play randomly from the array
 function computerPlay(){
     let str = ""
     let result = ["Rock", "Paper", "Scissors"];
@@ -7,5 +8,19 @@ function computerPlay(){
     }
     return str;
 }
+// Plays a single round of Rock, Paper or Scissors
+// function singleRound(playerSelection, computerSelection){
+//     computerSelection = computerPlay();
+//     playerSelection = " ";
+    
 
-console.log(computerPlay());
+// }
+// Lets the user input what to play with (Rock, Paper or Scissors)
+function playerSelection(answer){
+    answer = prompt("Please select either Rock, Paper or Scissors");
+    return answer;
+}
+
+//console.log(singleRound(playerSelection(),computerPlay()));
+//console.log(playerSelection());
+//console.log(computerPlay());
