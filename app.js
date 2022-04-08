b0ba021b22234cb93bb8630ff60eb7b93d155d7a
Bergulff <Bergulff@hotmail.com>
2022-04-08 20:08:50 +0200

    added computerPlay function

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 function computerPlay(){
-
+    let str = ""
+    let result = ["Rock", "Paper", "Scissors"];
+    
+    for (let i = 0; i < result.length; i++){
+        str = result[Math.floor(Math.random() * result.length)];
+    }
+    return str;
 }
-//Your game is going to play against the computer, so begin with a function called computerPlay
-//that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’. We’ll use this function in the game to make the computer’s play. 
-//Tip: use the console to make sure this is returning the expected output before moving to the next step!
+
+console.log(computerPlay());
