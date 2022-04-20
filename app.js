305fde2b8636c23cca6c205965ba5bd7bfaf367e
Bergulff <Bergulff@hotmail.com>
2022-04-20 14:40:50 +0200

    added game function

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ function computerPlay(){
 // Plays a single round of Rock, Paper or Scissors
 function playRound(playerSelection, computerSelection){
     
-    computerSelection = computerPlay();
-    playerSelection = playerChoice();
+    //computerSelection = computerPlay();
+    //playerSelection = playerChoice();
     
     if (computerSelection === playerSelection){
         console.log(`Computer choose: ${computerSelection}`);
@@ -25,23 +25,23 @@ function playRound(playerSelection, computerSelection){
         console.log("Draw");
         computerScore++;
         playerScore++;
-        console.log(`Your score and is: ${playerScore}. The computer score is: ${computerScore}`);
+        //console.log(`Your score and is: ${playerScore}. The computer score is: ${computerScore}`);
     }
     else if((computerSelection === "Rock" && playerSelection === "Scissors") || 
       (computerSelection === "Scissors" && playerSelection === "Paper") ||
       (computerSelection === "Paper" && playerSelection === "Rock")){
         console.log(`Computer choose: ${computerSelection}`);
         console.log(`U choose: ${playerSelection}`);
-        console.log("Computer won this round");
+        console.log(`You lose this round! ${computerSelection} beats ${playerSelection}`);
         computerScore++;
-        console.log(`The Computerscore is: ${computerScore}`);
+        //console.log(`The Computerscore is: ${computerScore}`);
     }
     else{
         console.log(`Computer choose: ${computerSelection}`);
         console.log(`U choose: ${playerSelection}`);
-        console.log("You won this round");
+        console.log(`You win this round! ${playerSelection} beats ${computerSelection}`);
         playerScore++;
-        console.log(`Your score is: ${playerScore}`);
+       //console.log(`Your score is: ${playerScore}`);
     }
 
     
@@ -53,19 +53,50 @@ function playerChoice(){
 }
 // Lets u play the game
 function game(){
+    let gameWinner = "none"
+    playerSelection = playerChoice();
+    computerSelection = computerPlay();
 
-    playRound(); 
+    for (let i = 1; i <= 5; i++){
+        let currentWinner = playRound(playerChoice(), computerPlay());
+        //console.log(playerChoice());
+
+        if(currentWinner === playerSelection){
+            playerScore++;
+        }
+        else if(currentWinner === computerSelection){
+            computerScore++;
+        }
     
-    if(computerScore >= 5){
-        console.log(`Computer won the game: ${computerScore}`);
+
+    if(playerScore > computerScore){
+        gameWinner = "Player";
     }
-    else if(playerScore >= 5){
-        console.log(`You won the game: ${playerScore}`);
+    else if(computerScore > playerScore){
+        gameWinner = "Computer";
+    }
+    else {
+        gameWinner = "Draw";
     }
 
+    console.log(`Player: ${playerScore} | Computer: ${computerScore}`);
+
+    if(playerScore === 5){
+        console.log(`Player won the game with: ${playerScore} to ${computerScore}`);
+    }
+    else if(computerScore === 5){
+        console.log(`Computer won the game with: ${computerScore} to ${playerScore}`);
+    }
+
+    if(gameWinner === "Draw"){
+        console.log("There is no winner, it's a draw \n\n");
+    }
+    else{
+        console.log(`Winner of the match is: ${gameWinner} \n\n`);
+    }
+
+ }
+ return gameWinner;
 }
 
-
-//console.log(computerPlay())
-//console.log(game())
 console.log(game());
