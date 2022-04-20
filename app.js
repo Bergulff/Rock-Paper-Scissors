8301dadc125bdf74b8d3925099dda3ec5e31d131
Bergulff <Bergulff@hotmail.com>
2022-04-20 10:19:06 +0200

    Logic to game is near done

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 // Returning the computers play randomly from the array
 
 const values = ["Rock", "Paper", "Scissors"];
+let playerScore = 0;
+let computerScore = 0;
+
 
 function computerPlay(){
 
@@ -9,28 +12,38 @@ function computerPlay(){
     return gameValues;
 }
 
-//let playerSelection = playerChoice().toLowerCase();
-
-
 
 // Plays a single round of Rock, Paper or Scissors
 function playRound(playerSelection, computerSelection){
     
     computerSelection = computerPlay();
     playerSelection = playerChoice();
-   
     
     if (computerSelection === playerSelection){
+        console.log(`Computer choose: ${computerSelection}`);
+        console.log(`U choose: ${playerSelection}`);
         console.log("Draw");
+        computerScore++;
+        playerScore++;
+        console.log(`Your score and is: ${playerScore}. The computer score is: ${computerScore}`);
     }
     else if((computerSelection === "Rock" && playerSelection === "Scissors") || 
       (computerSelection === "Scissors" && playerSelection === "Paper") ||
       (computerSelection === "Paper" && playerSelection === "Rock")){
-      console.log("Computer won this round");
+        console.log(`Computer choose: ${computerSelection}`);
+        console.log(`U choose: ${playerSelection}`);
+        console.log("Computer won this round");
+        computerScore++;
+        console.log(`The Computerscore is: ${computerScore}`);
     }
     else{
+        console.log(`Computer choose: ${computerSelection}`);
+        console.log(`U choose: ${playerSelection}`);
         console.log("You won this round");
+        playerScore++;
+        console.log(`Your score is: ${playerScore}`);
     }
+
     
 }
 // Lets the user input what to play with (Rock, Paper or Scissors)
@@ -38,18 +51,21 @@ function playerChoice(){
     let answer = prompt("Please select either Rock, Paper or Scissors");
     return answer;
 }
+// Lets u play the game
+function game(){
 
-// function game(){
+    playRound(); 
+    
+    if(computerScore >= 5){
+        console.log(`Computer won the game: ${computerScore}`);
+    }
+    else if(playerScore >= 5){
+        console.log(`You won the game: ${playerScore}`);
+    }
 
-//     let rounds = [5];
-
-//     for (let i = 1; i >= rounds.length; i++){
-//         console.log(rounds[i]);
-//     }
-//     playRound();
-// }
+}
 
 
 //console.log(computerPlay())
 //console.log(game())
-console.log(playRound());
+console.log(game());
