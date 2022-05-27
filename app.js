b24c1197209d58f834b40759a1bb2b8bbd58f1d1
Bergulff <Bergulff@hotmail.com>
2022-05-27 09:09:52 +0200

    Added buttons, and onclick feature

--- app.js
+++ app.js
@@ -4,72 +4,64 @@ const values = ["Rock", "Paper", "Scissors"];
 let playerScore = 0;
 let computerScore = 0;
 
+const result = document.querySelector("result");
+const score = document.querySelector("score");
 
+function computerPlay() {
+  gameValues = values[Math.floor(Math.random() * values.length)];
 
-const rockBtn = document.querySelector("#Rock");
-const paperBtn = document.querySelector("#Paper");
-const scissorsBtn = document.querySelector("#Scissors");
-// const playBtn = document.querySelector("#playGame")
-
-// rockBtn.addEventListener("click", () => {
-//     playerSelection = playerChoice();
-//     computerSelection = computerPlay();
-//     playRound(playerSelection, computerSelection);
-//     console.log(`${playerSelection}`);
-    
-// });
-
-function computerPlay(){
-
-    gameValues = values[Math.floor(Math.random() * values.length)];
-    
-    return gameValues.toLowerCase();
+  return gameValues;
 }
-
 
 // Plays a single round of Rock, Paper or Scissors
-function playRound(playerSelection, computerSelection){
-    
-    //computerSelection = computerPlay();
-    //playerSelection = playerChoice();
-    
-    if (computerSelection === playerSelection){
-        console.log(`Computer choose: ${computerSelection}`);
-        console.log(`U choose: ${playerSelection}`);
-        console.log("Draw");
-        computerScore++;
-        playerScore++;
-        //console.log(`Your score and is: ${playerScore}. The computer score is: ${computerScore}`);
-    }
-    else if((computerSelection === "rock" && playerSelection === "scissors") || 
-      (computerSelection === "scissors" && playerSelection === "paper") ||
-      (computerSelection === "paper" && playerSelection === "rock")){
-        console.log(`Computer choose: ${computerSelection}`);
-        console.log(`U choose: ${playerSelection}`);
-        console.log(`You lose this round! ${computerSelection} beats ${playerSelection}`);
-        computerScore++;
-        //console.log(`The Computerscore is: ${computerScore}`);
-    }
-    else{
-        console.log(`Computer choose: ${computerSelection}`);
-        console.log(`U choose: ${playerSelection}`);
-        console.log(`You win this round! ${playerSelection} beats ${computerSelection}`);
-        playerScore++;
-       //console.log(`Your score is: ${playerScore}`);
-    }
+function playRound(playerSelection, computerSelection) {
+  //computerSelection = computerPlay();
+  //playerSelection = playerChoice();
 
-    
+  if (computerSelection === playerSelection) {
+    console.log(`Computer choose: ${computerSelection}`);
+    console.log(`U choose: ${playerSelection}`);
+    console.log("Draw");
+
+    computerScore++;
+    playerScore++;
+  } else if (
+    (computerSelection === "rock" && playerSelection === "scissors") ||
+    (computerSelection === "scissors" && playerSelection === "paper") ||
+    (computerSelection === "paper" && playerSelection === "rock")
+  ) {
+    console.log(`Computer choose: ${computerSelection}`);
+    console.log(`U choose: ${playerSelection}`);
+    console.log(
+      `You lose this round! ${computerSelection} beats ${playerSelection}`
+    );
+    computerScore++;
+  } else {
+    console.log(`Computer choose: ${computerSelection}`);
+    console.log(`U choose: ${playerSelection}`);
+    console.log(
+      `You win this round! ${playerSelection} beats ${computerSelection}`
+    );
+    playerScore++;
+  }
 }
+
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    //console.log(button.id);
+    playerSelection = button.id;
+    computerSelection = computerPlay();
+    playRound(playerSelection, computerSelection);
+    isGameOver();
+  });
+});
 // Lets the user input what to play with (Rock, Paper or Scissors)
-//function playerChoice(){
-  
-    //let answer = prompt("Please select either Rock, Paper or Scissors");
+// function playerChoice(){
+// }
 
-  // return answer.toLowerCase();
-//}
-
-function isGameOver(){
-    return playerScore === 5 || computerScore === 5;
+function isGameOver() {
+  return playerScore === 5 || computerScore === 5;
 }
 
 // Lets u play the game
@@ -88,7 +80,6 @@ function isGameOver(){
 //         else if(currentWinner === computerSelection){
 //             computerScore++;
 //         }
-    
 
 //     if(playerScore > computerScore){
 //         gameWinner = "Player";
